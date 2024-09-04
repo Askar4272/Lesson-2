6ee046c99b8a909572ea509f632b0ff0316baed0
Askar4272 <askar4272@gmail.com>
2024-09-04 21:27:07 +0500

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,5 @@
+Quantity_of_homework = 12
+Time_spent = 1.5
+Course_name = 'Python'
+Time_per_task = Time_spent / Quantity_of_homework
+print(Course_name,', всего задач:', Quantity_of_homework, ', затраченное время:', Time_spent,', среднее время выполнения:', Time_per_task)
